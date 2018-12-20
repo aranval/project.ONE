fda600a1dd311220807aeecb6b0b1571e308138e
aranval <pjgiemza@gmail.com>
2018-12-20 12:50:17 +0100

    Placeholder main menu

---
+++ Source/MainMenuMain.cs
@@ -0,0 +1,50 @@
+    using Godot;
+using System;
+
+public class MainMenuMain : Node
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+    TextureButton start;
+    TextureButton cont;
+    TextureButton quit;
+
+    public override void _Ready()
+    {
+        start = (TextureButton) GetChild(1).GetChild(0).GetChild(1).GetNode("Start");
+        start.GrabFocus();
+        
+    }
+
+   public override void _PhysicsProcess(float delta)
+   {
+       cont = (TextureButton) GetChild(1).GetChild(0).GetChild(1).GetNode("Continue");
+       start = (TextureButton) GetChild(1).GetChild(0).GetChild(1).GetNode("Start");
+       quit = (TextureButton) GetChild(1).GetChild(0).GetChild(1).GetNode("Quit");
+
+       if(cont.IsHovered()) {
+           cont.GrabFocus();
+       }
+
+       if(start.IsHovered()) {
+           start.GrabFocus();
+       }
+
+       if(quit.IsHovered()) {
+           quit.GrabFocus();
+       }
+       
+   }
+
+   public void _OnContinuePressed() {
+       GD.Print("Continue");
+   }
+
+   public void _OnStartPressed() {
+       GetTree().ChangeScene("Scenes\\StageOne.tscn");
+   }
+   public void _OnQuitPressed() {
+       GetTree().Quit();
+   }
+}
